8a59b31e49c27896e7479c72b1843eeaa08f0093
RiverXu <sdxujianghe@126.com>
2016-09-17 21:26:51 +0800

    Debug: cannot get response from login.php

--- WebContent/blog-dev/api/login.php
+++ WebContent/blog-dev/api/login.php
@@ -17,16 +17,20 @@
         echo "invalid";
     }
 */
-
-    $conn = get_mysql_conn();
-    select_webdb();
-    $granted = verify_user($username,$password);
-    if ($granted=="invalid") {
-        echo "invalid";
-    } else {
-        buf_sid(SID,$granted);
+    try {
+        $conn = get_mysql_conn();
+        select_webdb();
+        $granted = verify_user($username,$password);
+        if ($granted!="invalid") {
+            buf_sid(session_id(),$granted);
+        }
         echo $granted;
+        close_mysql($conn);
+    } catch(Exception $e) {
+        echo "Exception!!".$e->getMessage();
+        echo "File:".$e->getFile();
+        echo "Line:".$e->getLine();
     }
-    close_mysql($conn);
+    
 
 ?>
