afe62d684e1a27ac740c4c2684899763dccc33f8
RiverXu <sdxujianghe@126.com>
2016-09-17 21:26:51 +0800

    Debug: cannot get response from login.php--2

--- WebContent/blog-dev/api/login.php
+++ WebContent/blog-dev/api/login.php
@@ -1,32 +1,27 @@
 <?php
     session_start();
-    include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/mysql.php");
-    include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-verification.php");
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    /*
-    //Stub implementation of user verification
 
-    if (!empty($username)&&!empty($password)) {
-        if ($username=="admin"&&$password=="123456") {
-            echo "admin";
-        } else {
-            echo "guest";
+
+    try {
+        include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/mysql.php");
+        include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-verification.php");
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+
+        $conn = get_mysql_conn();
+        select_webdb();
+        $granted = verify_user($username,$password);
+        if ($granted!="invalid") {
+            buf_sid(session_id(),$granted);
         }
-    } else {
-        echo "invalid";
-    }
-*/
-
-    $conn = get_mysql_conn();
-    select_webdb();
-    $granted = verify_user($username,$password);
-    if ($granted=="invalid") {
-        echo "invalid";
-    } else {
-        buf_sid(SID,$granted);
         echo $granted;
+        close_mysql($conn);
+    } catch(Exception $e) {
+        echo "Exception!!".$e->getMessage();
+        echo "File:".$e->getFile();
+        echo "Line:".$e->getLine();
     }
-    close_mysql($conn);
+    
 
 ?>
