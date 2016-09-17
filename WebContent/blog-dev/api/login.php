1d67542ad06f4c4c4afb446733d92abb0f3ea53f
RiverXu <sdxujianghe@126.com>
2016-09-17 21:30:46 +0800

    Debug: use try-catch to detect exceptions

--- WebContent/blog-dev/api/login.php
+++ WebContent/blog-dev/api/login.php
@@ -1,23 +1,14 @@
 <?php
     session_start();
-    include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/mysql.php");
-    include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-verification.php");
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    /*
-    //Stub implementation of user verification
 
-    if (!empty($username)&&!empty($password)) {
-        if ($username=="admin"&&$password=="123456") {
-            echo "admin";
-        } else {
-            echo "guest";
-        }
-    } else {
-        echo "invalid";
-    }
-*/
+
     try {
+        include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/mysql.php");
+        include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-verification.php");
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+
         $conn = get_mysql_conn();
         select_webdb();
         $granted = verify_user($username,$password);
