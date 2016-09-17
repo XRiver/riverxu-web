c8a65e7fa23e55f4e159f1c686256746968fb9bc
RiverXu <sdxujianghe@126.com>
2016-09-17 21:40:33 +0800

    Debug: step by step

--- WebContent/blog-dev/api/login.php
+++ WebContent/blog-dev/api/login.php
@@ -2,26 +2,20 @@
     session_start();
 
 
-    try {
-        include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/mysql.php");
-        include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-verification.php");
-        $username = $_POST["username"];
-        $password = $_POST["password"];
+    include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/mysql.php");
+    include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-verification.php");
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
+    echo "admin";
+    exit;
 
-        $conn = get_mysql_conn();
-        select_webdb();
-        $granted = verify_user($username,$password);
-        if ($granted!="invalid") {
-            buf_sid(session_id(),$granted);
-        }
-        echo $granted;
-        close_mysql($conn);
-    } catch(Exception $e) {
-        echo "Exception!!".$e->getMessage();
-        echo "File:".$e->getFile();
-        echo "Line:".$e->getLine();
+    $conn = get_mysql_conn();
+    select_webdb();
+    $granted = verify_user($username,$password);
+    if ($granted!="invalid") {
+        buf_sid(session_id(),$granted);
     }
-    
-
+    echo $granted;
+    close_mysql($conn);
 ?>
