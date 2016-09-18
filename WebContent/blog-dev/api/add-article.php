c90900a0323360d0f7c743006af9ce4449a22e8b
RiverXu <sdxujianghe@126.com>
2016-09-18 21:09:28 +0800

    Debug: use miltiple try-catch block to detect bug

--- WebContent/blog-dev/api/add-article.php
+++ WebContent/blog-dev/api/add-article.php
@@ -1,10 +1,16 @@
 <?php
 
+    try {
+        include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/mysql.php");
+        include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-verification.php");
+        include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-article-management.php");
+    } catch (Exception $e) {
+        echo "Error including libs";
+        exit;
+    }
+    
 
-    include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/mysql.php");
-    include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-verification.php");
-    include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-article-management.php");
-
+    
     session_start();
 
     $title = $_POST["title"];
@@ -15,26 +21,63 @@
         exit;
     }
 
-    $conn = get_mysql_conn();
-    select_webdb();
+    $conn = null;
+    try {
+        global $conn;
+        $conn = get_mysql_conn();
+        select_webdb();
 
-    $privilege = lookup_sid(session_id());
-    if ($privilege=="admin") {
-        echo "admin privilege detected...\n";
-        $id = addArticle($title,$content);
-        if($id!=-1) {
-            echo "Upload success! The article's id is ".$id;
-        } else {
-            echo "Internal error. Upload failed."
-        }
-    } else {
-        if ($privilege=="overtime") {
-            echo "Login overtime! Try login again";
-        } else {
-            echo "You don't have the admin privilege to upload an article! Or you can try login again.";
-        }
+    } catch (Exception $e) {
+        echo "Error connectiong to MySQL";
+        exit;
     }
 
-    close_mysql($conn);
+    $privilege = null;
+    try {
+        global $privilege;
+        $privilege = lookup_sid(session_id());
+
+    } catch (Exception $e) {
+        echo "Error finding the privilege";
+        exit;
+    }
+
+    try {
+        global $privilege;
+        if ($privilege=="admin") {
+            echo "admin privilege detected...\n";
+            $id = addArticle($title,$content);
+            if($id!=-1) {
+                echo "Upload success! The article's id is ".$id;
+            } else {
+                echo "Internal error. Upload failed."
+            }
+        } else {
+            if ($privilege=="overtime") {
+                echo "Login overtime! Try login again";
+            } else {
+                echo "You don't have the admin privilege to upload an article! Or you can try login again.";
+            }
+        }
+
+    } catch (Exception $e) {
+        echo "Priv is ".$privilege;
+        echo "Error adding articles";
+        exit;
+    }
+
+    try {
+        global $conn;
+        close_mysql($conn);
+
+    } catch (Exception $e) {
+        echo "Error disconnectiong";
+        exit;
+    }
+
+    
+
+    
+
 
 ?>
