bc516424142a4d6144fa96504464ac06c6e615c7
RiverXu <sdxujianghe@126.com>
2016-09-18 20:42:15 +0800

    Debug: detect bug's position--2
    buf_sid() function worked well; maybe the bug is in lookup_sid()

--- WebContent/blog-dev/api/add-article.php
+++ WebContent/blog-dev/api/add-article.php
@@ -10,14 +10,22 @@
 
     if (empty($title)||empty($content)) 
         echo "No content/title found.";
-        exit;
+        exit();
     }
 
     $conn = get_mysql_conn();
     select_webdb();
 
+    echo "Connected to databse...\n";
+    /////////
+    exit();
+    /////////
     $privilege = lookup_sid(session_id());
     if ($privilege=="admin") {
+        echo "admin privilege detected...\nexiting";
+        //////////
+        exit();
+        //////////
         $id = addArticle($title,$content);
         if($id!=-1) {
             echo "Upload success! The article's id is ".$id;
