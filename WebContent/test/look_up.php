49d6c989f1a574a2e1144e9e4c7072a312f224bc
RiverXu <sdxujianghe@126.com>
2016-09-19 08:29:11 +0800

    Forgot to init DB connection.

--- WebContent/test/look_up.php
+++ WebContent/test/look_up.php
@@ -1,8 +1,13 @@
 <?php
-
+    include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/mysql.php");
     include($_SERVER["DOCUMENT_ROOT"]."/WEB-INF/php-lib/blog-verification.php");
+
     session_start();
+    $c = get_mysql_conn();
+    select_webdb();
+
     $privilege = lookup_sid(session_id());
     echo $privilege;
+    close_mysql($c);
 
 ?>
